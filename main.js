7bdb473fb903dde4d1eba11754f149bace2889b5
deltechdesigns <deltechdesigns@gmail.com>
2022-11-25 06:20:12 -0800

    completing the landing page for both large screen and small screen

--- main.js
+++ main.js
@@ -3,10 +3,36 @@ const navBar = document.querySelector('.nav-bar');
 const closeBtn = document.querySelector('.close-btn'); 
 
 hideBar.onclick = () => {
-    navBar.style.width = '250px'; 
+    navBar.style.width = '300px'; 
 }
 
 closeBtn.onclick = () => {
     navBar.style.width = '0px'; 
 
 }
+
+
+/* Landing page logo */
+
+const containerLogo = [
+    {pic: "./img/Logo.png"}, 
+    {pic: "./img/presto logo.png"}, 
+    {pic: "./img/Logo.png"},  
+    {pic: "./img/presto logo.png"}, 
+    {pic: "./img/Logo.png"}, 
+    {pic: "./img/presto logo.png"}, 
+]; 
+
+
+let containerLogoItem = document.querySelector('.logo-container');
+
+containerLogo.map( ({pic}) => {
+    containerLogoItem.innerHTML += `
+        <div class="landingpage-logo">
+            <img src="${pic}" alt=""> 
+        </div>
+    `
+    console.log(pic);
+})
+
+// console.log(landingLogo);
